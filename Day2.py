6635b5b903455e886748a250496d33715275dc10
Siba0718 <vtarunakumarrao@gmail.com>
2022-07-04 23:34:11 +0530

    Operators

--- Day2.py
+++ Day2.py
@@ -5,13 +5,17 @@
 # namet_s = str(num_name)
 # print("Your name has "+namet_s+" character. "
 
-
-# 🚨 Don't change the code below 👇
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-# 🚨 Don't change the code above 👆
-height1 = float(height)
-weight1 = float(weight)
-#Write your code below this line 👇
-BMI = weight1/(height1**2)
-print(int(BMI))
+#
+# # 🚨 Don't change the code below 👇
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# # 🚨 Don't change the code above 👆
+# height1 = float(height)
+# weight1 = float(weight)
+# #Write your code below this line 👇
+# BMI = weight1/(height1**2)
+# print(int(BMI))
+#......................f-String.....................
+a=10
+b=11.88
+print(f"Hello ,here is your number{a}   {b}")
