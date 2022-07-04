f7ff2e3c4183139f0e6c6834ca382bcd72e59f5c
Siba0718 <vtarunakumarrao@gmail.com>
2022-07-04 23:13:05 +0530

    Operators

--- Day2.py
+++ Day2.py
@@ -1,6 +1,17 @@
-#typeCasting and more
-name = input("Input your name")
-num_name = len(name)
-# print(type(num_name))
-namet_s = str(num_name)
-print("Your name has "+namet_s+" character. ")
+# #typeCasting and more
+# name = input("Input your name")
+# num_name = len(name)
+# # print(type(num_name))
+# namet_s = str(num_name)
+# print("Your name has "+namet_s+" character. "
+
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+height1 = float(height)
+weight1 = float(weight)
+#Write your code below this line 👇
+BMI = weight1/(height1**2)
+print(int(BMI))
