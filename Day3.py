6cab4147b837a4980251e2403684b9e8cd467d6d
Siba0718 <vtarunakumarrao@gmail.com>
2022-07-06 19:29:22 +0530

    Pizza Price

--- Day3.py
+++ Day3.py
@@ -1,3 +1,5 @@
+
+"""
 # 🚨 Don't change the code below 👇
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
@@ -15,3 +17,60 @@ elif(result>30 and result<35):
     print(f"Your BMI is {result},you are obese.")
 else:
     print(f"Your BMI is {result},you are clinically obese.")
+    """
+
+
+""""
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+sPizza=15
+mPizza=20
+lPizza=25
+if(add_pepperoni == "Y"):
+    sPizza += 2
+    mPizza += 3
+    lPizza += 3
+if(extra_cheese=="Y"):
+    sPizza += 1
+    mPizza += 1
+    lPizza += 1
+
+if(size == "L"):
+    print(f"Your final bill is: ${lPizza}")
+if(size == "S"):
+    print(f"Your final bill is: ${sPizza}")
+if(size == "M"):
+    print(f"Your final bill is: ${mPizza}")
+    
+                                """
+
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+cost = 0
+
+if(size == "S"):
+    cost += 15
+if(size == "L"):
+    cost += 25
+if(size == "M"):
+    cost += 20
+
+if(add_pepperoni == "Y"):
+    if (size == "S"):
+        cost += 2
+    elif(size == "M" or size == "L"):
+        cost += 3
+if(extra_cheese == "Y"):
+    cost += 1
+print(f"Your final bill is: ${cost}")
+
