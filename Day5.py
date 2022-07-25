af134d1ebb58676850305cb516315039774434b3
Siba0718 <vtarunakumarrao@gmail.com>
2022-07-25 16:16:14 +0530

    Day 5 !!! Delay Due To Exam

---
+++ Day5.py
@@ -0,0 +1,41 @@
+# fruits = ["apple","banana","Peach","Pear"]
+# for fruit in fruits:
+#     print(fruit)
+# print("Hello")
+
+
+
+"""
+student_height = input("Input Student Height").split()
+print(student_height)
+count = 0
+for n in range(0,len(student_height)):
+    student_height[n] = int(student_height[n])
+
+# for n in student_height:
+sum = 0
+for i in student_height:
+    sum += i
+    count = count + 1
+avg = round(sum/count)
+print(avg)
+
+"""
+
+"""
+numbers = input("Enter the numbers").split()
+for n in range(0,len(numbers)):
+    numbers[n] = int(numbers[n])
+max_value = 0
+for x in numbers:
+    if(x>max_value):
+        max_value = x
+print(max_value)
+
+"""
+
+
+
+
+
+
